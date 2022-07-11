51429ebb5fa955eb384dc85e12ca4a419dc69d30
Tomer Zait <realgam3@gmail.com>
2022-07-11 21:35:45 +0300

    add k8s object name to patch function

--- kompose_ex/api.py
+++ kompose_ex/api.py
@@ -80,15 +80,15 @@ def patch_from_yaml(k8s_client, yml_object, verbose=False, **kwargs):
         version = group
         group = "core"
     group = "".join(group.rsplit(".k8s.io", 1))
-    group = "".join(word.capitalize() for word in group.split('.'))
+    group = "".join(word.capitalize() for word in group.split("."))
     fcn_to_call = "{0}{1}Api".format(group, version.capitalize())
     k8s_api = getattr(client, fcn_to_call)(k8s_client)
     kind = yml_object["kind"]
-    kind = UPPER_FOLLOWED_BY_LOWER_RE.sub(r'\1_\2', kind)
-    kind = LOWER_OR_NUM_FOLLOWED_BY_UPPER_RE.sub(r'\1_\2', kind).lower()
+    kind = UPPER_FOLLOWED_BY_LOWER_RE.sub(r"\1_\2", kind)
+    kind = LOWER_OR_NUM_FOLLOWED_BY_UPPER_RE.sub(r"\1_\2", kind).lower()
     if "name" in yml_object["metadata"]:
         name = yml_object["metadata"]["name"]
-        kwargs['name'] = name
+        kwargs["name"] = name
 
     # Expect the user to patch namespaced objects more often
     if hasattr(k8s_api, "patch_namespaced_{0}".format(kind)):
@@ -96,16 +96,16 @@ def patch_from_yaml(k8s_client, yml_object, verbose=False, **kwargs):
         # if any
         if "namespace" in yml_object["metadata"]:
             namespace = yml_object["metadata"]["namespace"]
-            kwargs['namespace'] = namespace
+            kwargs["namespace"] = namespace
         resp = getattr(k8s_api, "patch_namespaced_{0}".format(kind))(
             body=yml_object, **kwargs)
     else:
-        kwargs.pop('namespace', None)
+        kwargs.pop("namespace", None)
         resp = getattr(k8s_api, "patch_{0}".format(kind))(
             body=yml_object, **kwargs)
     if verbose:
         msg = "{0} patched.".format(kind)
-        if hasattr(resp, 'status'):
+        if hasattr(resp, "status"):
             msg += " status='{0}'".format(str(resp.status))
         print(msg)
     return resp
@@ -197,19 +197,21 @@ def patch(files_path, verbose=False, logger=None, **kwargs):
     if path.isdir(files_path):
         files = glob(path.join(files_path, "*"))
     for file_path in files:
+        with open(file_path, "r", encoding="UTF-8") as fr:
+            yaml_object = yaml.safe_load(fr)
+
+        object_name = get_object_name(yaml_object)
         try:
-            with open(file_path, "r", encoding="UTF-8") as fr:
-                yaml_obj = yaml.safe_load(fr)
-                patch_from_yaml(
-                    api_client,
-                    yml_object=yaml_obj,
-                    verbose=verbose,
-                    **kwargs
-                )
-            logger.info(f"Kubernetes file \"{file_path}\" patched")
+            patch_from_yaml(
+                api_client,
+                yml_object=yaml_object,
+                verbose=verbose,
+                **kwargs
+            )
+            logger.info(f"{object_name} patched")
         except FailToCreateError as ex:
             body = json.loads(ex.api_exceptions[0].body)
-            raise Exception(f"Kubernetes file \"{file_path}\" failed ({body['message']})")
+            raise Exception(f"{object_name} failed ({body['message']})")
 
 
 @utils.retries(10, delay=2)
