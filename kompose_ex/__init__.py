17e41412c4a39a94e2acfb4f1cbfdc9be41d8072
Tomer Zait <realgam3@gmail.com>
2022-07-12 15:14:56 +0300

    rmove quote form deletion message

--- kompose_ex/__init__.py
+++ kompose_ex/__init__.py
@@ -343,7 +343,7 @@ class KomposeEx(object):
                 )
                 if not is_file:
                     f_path = path.normpath(path.join(out_path, f"{service_name}-pod.{file_ext}"))
-                    self.logger.info(f"Kubernetes file {json.dumps(f_path)} deleted'")
+                    self.logger.info(f"Kubernetes file {json.dumps(f_path)} deleted")
 
                 items[f"{service_name}-cronjob"] = models.V1CronJob(
                     api_version="batch/v1",
