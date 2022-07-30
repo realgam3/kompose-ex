454fcd283b34fc382babffc1dfbe49fac7bb0811
Tomer Zait <realgam3@gmail.com>
2022-07-30 19:30:09 +0300

    remove string formats

--- kompose_ex/__init__.py
+++ kompose_ex/__init__.py
@@ -151,9 +151,9 @@ class KomposeEx(object):
             parser.error(
                 f"The Namespace {json.dumps(namespace)} is invalid: "
                 f"metadata.name: Invalid value: {json.dumps(namespace)}: "
-                f"a lowercase RFC 1123 label must consist of lower case alphanumeric characters or '-', "
-                f"and must start and end with an alphanumeric character (e.g. 'my-name',  or '123-abc', "
-                f"regex used for validation is '[a-z0-9]([-a-z0-9]*[a-z0-9])?')"
+                "a lowercase RFC 1123 label must consist of lower case alphanumeric characters or '-', "
+                "and must start and end with an alphanumeric character (e.g. 'my-name',  or '123-abc', "
+                "regex used for validation is '[a-z0-9]([-a-z0-9]*[a-z0-9])?')"
             )
 
         return args_known, args_unknown
